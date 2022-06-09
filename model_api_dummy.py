fc0516a7125cecf1a68b9309bad06b742904ff76
yusrilalbi <60544526+yusrilalbi@users.noreply.github.com>
2022-06-09 13:39:43 +0700

    Upload Necessary File (Not Testing Yet)

---
+++ model_api_dummy.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,jsonify
+import json
+
+app = Flask(__name__)
+
+with open("label.json") as f:
+    label = json.load(f)
+    
+@app.route('/')
+def index():
+    return "Hello World"
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    
+    return jsonify(label)
+if __name__ == '__main__':
+    app.run(debug=True)
